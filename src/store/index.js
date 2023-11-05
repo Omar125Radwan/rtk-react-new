4f343f9b9c6a27a157fe78a7f727e6e8064e6dad
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2023-11-06 01:10:58 +0200

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {}
+  }
+});
